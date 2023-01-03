6d5c1a1ece43c71dfe1ec3c716437d3ba8083f6a
Gianluigi Forte <koalakoker@gmail.com>
2023-01-03 18:25:27 +0100

    Mag (Mouse)

--- sketch.js
+++ sketch.js
@@ -13,7 +13,11 @@ function draw() {
   //line(0, 0, center.x, center.y);
 
   mouse.sub(center);
-  mouse.mult(0.5);
+
+  let h = mouse.mag();
+  fill(255);
+  rect(0, 0, h, 10);
+
   stroke(255);
   translate(width / 2, height / 2);
   line(0, 0, mouse.x, mouse.y);
