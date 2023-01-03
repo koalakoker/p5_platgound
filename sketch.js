86725ee55dfb468eac036f16d16ef30a84d09ac9
Gianluigi Forte <koalakoker@gmail.com>
2023-01-03 18:45:44 +0100

    Basic mover

--- sketch.js
+++ sketch.js
@@ -1,7 +1,42 @@
+class Mover {
+  constructor() {
+    this.position = createVector(width / 2, height / 2);
+    this.velocity = createVector(random(-2, 2), random(-2, 2));
+  }
+  update() {
+    this.position.add(this.velocity);
+  }
+  display() {
+    stroke(0);
+    fill(175);
+    ellipse(this.position.x, this.position.y, 16, 16);
+  }
+  checkEdge() {
+    if (this.position.x < 0) {
+      this.position.x = width;
+    }
+    if (this.position.x > width) {
+      this.position.x = 0;
+    }
+    if (this.position.y < 0) {
+      this.position.y = height;
+    }
+    if (this.position.y > height) {
+      this.position.y = 0;
+    }
+  }
+}
+
+let mover;
+
 function setup() {
   createCanvas(400, 400);
+  mover = new Mover();
 }
 
 function draw() {
   background(220);
+  mover.update();
+  mover.checkEdge();
+  mover.display();
 }
