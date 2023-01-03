205f38e4567ed0aac35cfb15a447a70aeacb64ec
Gianluigi Forte <koalakoker@gmail.com>
2023-01-03 18:20:42 +0100

    Subtract (Mouse)

--- sketch.js
+++ sketch.js
@@ -3,5 +3,17 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  let mouse = createVector(mouseX, mouseY);
+  let center = createVector(width / 2, height / 2);
+
+  //stroke(10);
+  //line(0, 0, mouse.x, mouse.y);
+  //line(0, 0, center.x, center.y);
+
+  mouse.sub(center);
+  stroke(255);
+  translate(width / 2, height / 2);
+  line(0, 0, mouse.x, mouse.y);
 }
