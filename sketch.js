5fe1128fdf77cf07d8b8a92439a23398f783ff69
Gianluigi Forte <koalakoker@gmail.com>
2023-01-06 11:56:22 +0100

    Diffenent mass, gravity and random wind

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 class Mover {
-  constructor() {
-    this.position = createVector(random(width), random(height));
+  constructor(x, y, m) {
+    this.position = createVector(x, y);
     this.velocity = createVector();
     this.acceleration = createVector();
     this.speedLimit = 10;
+    this.mass = m;
   }
   applyForce(force) {
+    force = p5.Vector.div(force, this.mass);
     this.acceleration.add(force);
   }
   update() {
@@ -17,21 +19,24 @@ class Mover {
   display() {
     stroke(0);
     fill(175);
-    ellipse(this.position.x, this.position.y, 16, 16);
+    ellipse(this.position.x, this.position.y, 16 * this.mass, 16 * this.mass);
   }
   checkEdge() {
     if (this.position.x < 0) {
-      this.position.x = width;
+      this.position.x = 0;
+      this.velocity.x *= -1;
     }
     if (this.position.x > width) {
-      this.position.x = 0;
+      this.position.x = width;
+      this.velocity.x *= -1;
     }
     if (this.position.y < 0) {
       this.position.y = 0;
       this.velocity.y *= -1;
     }
     if (this.position.y > height) {
-      this.position.y = 0;
+      this.position.y = height;
+      this.velocity.y *= -1;
     }
   }
 }
@@ -40,8 +45,8 @@ let movers = [];
 
 function setup() {
   createCanvas(400, 400);
-  for (let a = 0; a < 1; a++) {
-    movers.push(new Mover());
+  for (let a = 0; a < 5; a++) {
+    movers.push(new Mover(random(width), random(height), random(10)));
   }
 }
 
@@ -50,14 +55,14 @@ let t = 0;
 function draw() {
   background(220);
   movers.forEach((mover) => {
-    mover.applyForce(createVector(0, -0.1)); // Helium force
-    let maxWindForce = 0.1;
-    let wind = createVector(
-      map(noise(t), 0, 1, -maxWindForce, maxWindForce),
-      0
-    );
-    t += 0.1;
-    mover.applyForce(wind);
+    let gravity = createVector(0, 0.1);
+    mover.applyForce(gravity);
+
+    if (mouseIsPressed) {
+      let wind = createVector(0.1, 0);
+      mover.applyForce(wind);
+    }
+
     mover.update();
     mover.checkEdge();
     mover.display();
