b305cde828d66421086d163fab4a6a5a1340c930
Gianluigi Forte <koalakoker@gmail.com>
2023-01-08 18:21:48 +0100

    Base of Mover

--- sketch.js
+++ sketch.js
@@ -1,55 +1,20 @@
-class Mover {
-  constructor() {
-    this.position = createVector(random(width), random(height));
-    this.velocity = createVector();
-    this.acceleration = createVector();
-    this.speedLimit = 10;
-  }
-  update() {
-    let accAmpl = 0.5;
-    let target = createVector(mouseX, mouseY);
-    this.acceleration = p5.Vector.sub(target, this.position);
-    this.acceleration.setMag(accAmpl);
-
-    this.velocity.add(this.acceleration);
-    this.velocity.limit(this.speedLimit);
-    this.position.add(this.velocity);
-  }
-  display() {
-    stroke(0);
-    fill(175);
-    ellipse(this.position.x, this.position.y, 16, 16);
-  }
-  checkEdge() {
-    if (this.position.x < 0) {
-      this.position.x = width;
-    }
-    if (this.position.x > width) {
-      this.position.x = 0;
-    }
-    if (this.position.y < 0) {
-      this.position.y = height;
-    }
-    if (this.position.y > height) {
-      this.position.y = 0;
-    }
-  }
-}
-
 let movers = [];
 
 function setup() {
   createCanvas(400, 400);
-  for (let a = 0; a < 20; a++) {
-    movers.push(new Mover());
+  movers = new Array(20);
+  for (let i = 0; i < movers.length; i++) {
+    movers[i] = new Mover(random(width), random(height));
+    movers[i].velocity = createVector(random(10), random(10));
   }
 }
 
 function draw() {
-  background(220);
-  movers.forEach((mover) => {
+  background(0);
+  for (let i = 0; i < movers.length; i++) {
+    mover = movers[i];
     mover.update();
     mover.checkEdge();
     mover.display();
-  });
+  }
 }
