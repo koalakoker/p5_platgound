a9e2a70cbc6b11dfd8d52153a1fed8695cb6a066
Gianluigi Forte <koalakoker@gmail.com>
2023-01-10 18:47:59 +0100

    Example of paragraph

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
+let p;
+let count = 0.0;
+
 function setup() {
   createCanvas(400, 400);
+  p = createP();
 }
 
 function draw() {
+  count += deltaTime / 1000;
   background(220);
+  p.html("Time:" + nf(count, 0, 2));
 }
