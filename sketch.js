1b9ff9f3902163fa036ad7e5b692c0302a6dce76
Gianluigi Forte <koalakoker@gmail.com>
2023-01-10 18:59:22 +0100

    Slider example

--- sketch.js
+++ sketch.js
@@ -1,13 +1,20 @@
-let p;
+let p, s;
 let count = 0.0;
 
 function setup() {
   createCanvas(400, 400);
   p = createP();
+  s = createSlider(0, 1, 0.5, 0.01);
+  s.max = 2;
+  rectMode(CENTER);
 }
 
 function draw() {
-  count += deltaTime / 1000;
-  background(220);
-  p.html("Time:" + nf(count, 0, 2));
+  count = s.value();
+  background(0);
+  p.html("Slider:" + nf(count, 0, 2));
+  stroke(255);
+  fill(200);
+  rect;
+  rect(count * width, height - 50, 30, 10);
 }
