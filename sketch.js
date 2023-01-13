abb76d6e236247e92071e053ef9cf492028af29d
Gianluigi Forte <koalakoker@gmail.com>
2023-01-13 06:51:16 +0100

    Adding sel to select the state

--- sketch.js
+++ sketch.js
@@ -1,9 +1,35 @@
 let drawElement = [];
 let dragging = false;
-let state = new stateAddCircle();
+let state;
 
 function setup() {
   createCanvas(400, 400);
+  sel = createSelect();
+  sel.option("");
+  sel.option("addLine");
+  sel.option("addCircle");
+  sel.option("addRect");
+  sel.selected("");
+  sel.changed(changeMode);
+}
+
+function changeMode() {
+  let selected = sel.value();
+  switch (selected) {
+    case "addLine":
+      state = new stateAddLine();
+      break;
+    case "addCircle":
+      state = new stateAddCircle();
+      break;
+    case "addRect":
+      state = new stateAddRect();
+      break;
+
+    default:
+      state = null;
+      break;
+  }
 }
 
 function draw() {
@@ -12,21 +38,31 @@ function draw() {
     const element = drawElement[i];
     element.draw();
   }
-  state.draw();
+  if (state) {
+    state.draw();
+  }
 }
 
 function mousePressed() {
-  dragging = true;
-  state.mousePressed();
+  if (state) {
+    if (mouseX > 0 && mouseY > 0 && mouseX < width && mouseY < height) {
+      dragging = true;
+      state.mousePressed();
+    }
+  }
 }
 
 function mouseReleased() {
-  dragging = false;
-  drawElement.push(state.mouseReleased());
+  if (state) {
+    dragging = false;
+    drawElement.push(state.mouseReleased());
+  }
 }
 
 function mouseDragged() {
-  if (dragging) {
-    state.mouseDragged();
+  if (state) {
+    if (dragging) {
+      state.mouseDragged();
+    }
   }
 }
