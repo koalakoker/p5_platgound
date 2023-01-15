2d0dd014e490e4746fad8b78e0b8eb1f6f6ad94d
Gianluigi Forte <koalakoker@gmail.com>
2023-01-15 17:48:08 +0100

    Creating 2D array

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let grid;
+let dim = 40;
+let rows, cols;
+
+function create2DArray(rows, cols) {
+  let grid = Array(cols);
+  for (let i = 0; i < grid.length; i++) {
+    let col = grid[i];
+    col = new Array(rows);
+  }
+  return grid;
+}
+
 function setup() {
   createCanvas(400, 400);
+  cols = width / dim;
+  rows = height / dim;
+  grid = create2DArray(rows, cols);
 }
 
 function draw() {
-  background(220);
+  background(0);
 }
