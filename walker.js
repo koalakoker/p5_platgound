999af3bfc4897ab751e25dc37545e7772064e26f
Gianluigi Forte <koalakoker@gmail.com>
2023-01-15 20:07:13 +0100

    Self avoiding work until stuck

--- walker.js
+++ walker.js
@@ -5,37 +5,44 @@ class Walker {
   draw() {
     noStroke();
     fill(255);
-    circle(this.pos.x * dim, this.pos.y * dim, dim - 4);
+    circle(dim / 2 + this.pos.x * dim, dim / 2 + this.pos.y * dim, dim - 4);
   }
   walk(grid) {
-    let options = this.checkOptions(grid, this.pos.x, this.pos.y);
+    this.grid = grid;
+    let options = this.checkOptions(this.pos.x, this.pos.y);
     if (options.length > 0) {
       let d = random(options);
       strokeWeight(4);
       stroke(255);
       beginShape();
-      vertex(this.pos.x * dim, this.pos.y * dim);
+      vertex(dim / 2 + this.pos.x * dim, dim / 2 + this.pos.y * dim);
       this.pos.x += d.dx;
       this.pos.y += d.dy;
-      vertex(this.pos.x * dim, this.pos.y * dim);
+      vertex(dim / 2 + this.pos.x * dim, dim / 2 + this.pos.y * dim);
       endShape();
     } else {
       print("I'm stuck!");
       noLoop();
     }
   }
-  checkOptions(grid, i, j) {
+  isValid(i, j) {
+    if (i < 0 || i >= cols || j < 0 || j >= rows) {
+      return false;
+    }
+    return !this.grid[i][j];
+  }
+  checkOptions(i, j) {
     let options = [];
-    if (!grid[i][j - 1]) {
+    if (this.isValid(i, j - 1)) {
       options.push({ dx: 0, dy: -1 });
     }
-    if (!grid[i][j + 1]) {
+    if (this.isValid(i, j + 1)) {
       options.push({ dx: 0, dy: 1 });
     }
-    if (!grid[i - 1][j]) {
+    if (this.isValid(i - 1, j)) {
       options.push({ dx: -1, dy: 0 });
     }
-    if (!grid[i + 1][j]) {
+    if (this.isValid(i + 1, j)) {
       options.push({ dx: 1, dy: 0 });
     }
     return options;
