43b5a2dda1b8987755408bc118b146ceee67fb47
Gianluigi Forte <koalakoker@gmail.com>
2023-01-15 18:16:21 +0100

    Walker + grid not checkd

--- sketch.js
+++ sketch.js
@@ -1,14 +1,13 @@
 let grid;
-let dim = 5;
+let dim = 20;
 let rows, cols;
 
 function create2DArray(rows, cols) {
-  let grid = Array(cols);
+  let grid = new Array(cols);
   for (let i = 0; i < grid.length; i++) {
-    let col = grid[i];
-    col = new Array(rows);
-    for (let j = 0; j < col.length; j++) {
-      col[j] = false;
+    grid[i] = new Array(rows);
+    for (let j = 0; j < grid[i].length; j++) {
+      grid[i][j] = false;
     }
   }
   return grid;
@@ -20,10 +19,21 @@ function setup() {
   rows = height / dim;
   grid = create2DArray(rows, cols);
   walker = new Walker(cols / 2, rows / 2);
+  grid[int(cols / 2)][int(rows / 2)] = true;
   background(0);
+  rectMode(CENTER);
 }
 
 function draw() {
   walker.draw();
   walker.walk();
+  grid[walker.pos.x][walker.pos.y] = true;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j]) {
+        fill(255, 0, 0, 10);
+        rect(i * dim, j * dim, dim, dim);
+      }
+    }
+  }
 }
