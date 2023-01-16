3d84940828fd2ab0f76cb4ef0aeaa34487f9b662
Gianluigi Forte <koalakoker@gmail.com>
2023-01-16 06:40:19 +0100

    Random grid value (true false) and draw as rectangle

--- sketch.js
+++ sketch.js
@@ -5,8 +5,10 @@ let rows, cols;
 function create2DArray(rows, cols) {
   let grid = Array(cols);
   for (let i = 0; i < grid.length; i++) {
-    let col = grid[i];
-    col = new Array(rows);
+    grid[i] = new Array(rows);
+    for (let j = 0; j < grid[i].length; j++) {
+      grid[i][j] = floor(random(2));
+    }
   }
   return grid;
 }
@@ -20,4 +22,12 @@ function setup() {
 
 function draw() {
   background(0);
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j]) {
+        fill(255);
+        rect(i * dim, j * dim, dim, dim);
+      }
+    }
+  }
 }
