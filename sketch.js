ea360339ec8d5c5f88dbbdf96da8a2cab4f1702a
Gianluigi Forte <koalakoker@gmail.com>
2023-01-21 20:05:02 +0100

    Multi actractors

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 let movers = [];
-let actractor;
+let actractors = [];
 
 function setup() {
   createCanvas(400, 400);
-
-  actractor = new createVector(200, 200);
-
   movers = new Array(20);
   for (let i = 0; i < movers.length; i++) {
     movers[i] = new Mover(random(width), random(height));
-    //movers[i].velocity = createVector();
+  }
+  actractors = new Array(6);
+  for (let i = 0; i < actractors.length; i++) {
+    actractors[i] = createVector(random(width), random(height));
   }
 }
 
@@ -18,11 +18,15 @@ function draw() {
 
   stroke(0, 255, 0);
   strokeWeight(4);
-  point(actractor.x, actractor.y);
+  for (let i = 0; i < actractors.length; i++) {
+    point(actractors[i].x, actractors[i].y);
+  }
 
   for (let i = 0; i < movers.length; i++) {
     mover = movers[i];
-    mover.actraction(actractor);
+    for (let i = 0; i < actractors.length; i++) {
+      mover.actraction(actractors[i]);
+    }
     mover.update();
     mover.display();
   }
