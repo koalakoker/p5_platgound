4c6a785f5858162e96c287d334b7e74505fd5e03
Gianluigi Forte <koalakoker@gmail.com>
2023-01-23 06:36:21 +0100

    Ex_3_1_Acc

--- sketch.js
+++ sketch.js
@@ -1,24 +1,22 @@
 let bastLen = 100;
-let rot = 0;
+let aAcceleration = 0.0001;
+let aVelocity = 0;
+let angle = 0;
 
 function setup() {
   createCanvas(400, 400);
+  background(0);
 }
 
 function draw() {
-  background(0);
   translate(200, 200);
-  rotate(rot);
-  stroke(255);
-  fill(100);
+  rotate(angle);
+  stroke(255, 255, 255, 50);
+  fill(100, 100, 100, 50);
   line(-bastLen / 2, 0, bastLen / 2, 0);
   circle(-bastLen / 2, 0, 16, 16);
   circle(bastLen / 2, 0, 16, 16);
 
-  if (keyIsDown(RIGHT_ARROW)) {
-    rot += 0.1;
-  }
-  if (keyIsDown(LEFT_ARROW)) {
-    rot -= 0.1;
-  }
+  aVelocity += aAcceleration;
+  angle += aVelocity;
 }
