f49bf1edc861663a8c49f5277af3d2cab0c2664f
Gianluigi Forte <koalakoker@gmail.com>
2023-01-24 06:54:30 +0100

    Spiral

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let r = 0;
+let theta = 0;
+
 function setup() {
   createCanvas(400, 400);
+  background(0);
 }
 
 function draw() {
-  background(220);
+  let x = cos(theta) * r;
+  let y = sin(theta) * r;
+  fill(255);
+  noStroke();
+  circle(width / 2 + x, height / 2 + y, 16);
+
+  theta += 0.01;
+  r += 0.05;
 }
