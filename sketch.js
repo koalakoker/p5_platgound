96b9887c26e906115f3a6821196bd22b041cc99b
Gianluigi Forte <koalakoker@gmail.com>
2023-01-24 07:35:52 +0100

    Harmonic

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+let amplitude = 0;
+let period = 120;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
+  amplitude = width / 2 - 30;
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  let x = amplitude * sin((TWO_PI * frameCount) / period);
+  fill(255);
+  noStroke();
+  circle(width / 2 - x, height / 2, 16);
 }
