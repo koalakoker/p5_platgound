165843f77edfd23c50f95edec7e32f8ca2081da6
Gianluigi Forte <koalakoker@gmail.com>
2023-02-09 17:24:29 +0100

    Line picker draws lines

--- gui/picker/linePicker.js
+++ gui/picker/linePicker.js
@@ -1,7 +1,15 @@
 class LinePicker extends Picker {
   constructor(x, y) {
     super(x, y);
+    this.hPick = 200;
     this.weight = 1;
+    this.weights = [];
+    this.weights.push(1);
+    this.weights.push(2);
+    this.weights.push(4);
+    this.weights.push(6);
+    this.weights.push(8);
+    this.weights.push(10);
   }
   display() {
     stroke(255);
@@ -18,7 +26,32 @@ class LinePicker extends Picker {
     );
     if (this.selected) {
       strokeWeight(1);
-      rect(this.basePoint().x, this.basePoint().y, this.side, this.side);
+      rect(this.basePoint().x, this.basePoint().y, this.side, this.hPick);
+      const l = this.weights.length;
+      const space = this.hPick / l;
+      for (let i = 0; i < l; i++) {
+        const y = space / 2 + this.basePoint().y + i * space;
+        strokeWeight(this.weights[i]);
+        line(
+          this.basePoint().x + 10,
+          y,
+          this.basePoint().x + this.side - 10,
+          y
+        );
+      }
     }
+  }
+  inside() {
+    return super.inside() || this.insidePicker();
+  }
+  insidePicker() {
+    return Rect.inside(
+      mouseX,
+      mouseY,
+      this.basePoint().x,
+      this.basePoint().y,
+      this.side,
+      this.hPick
+    );
   }
 }
