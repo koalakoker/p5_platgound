afba9abbf96c5963bb46ab6261f743a5b71fd231
Gianluigi Forte <koalakoker@gmail.com>
2023-02-09 14:48:33 +0100

    Select line working

--- elements/line.js
+++ elements/line.js
@@ -7,9 +7,39 @@ class Line extends Element {
     this.x2 = x2;
     this.y2 = y2;
   }
+  rotCCW(p, th) {
+    let t00 = cos(th);
+    let t11 = t00;
+    let t01 = sin(th);
+    let t10 = -t01;
+    return createVector(t00 * p.x + t01 * p.y, t10 * p.x + t11 * p.y);
+  }
+  rotCW(p, th) {
+    let t00 = cos(th);
+    let t11 = t00;
+    let t01 = -sin(th);
+    let t10 = -t01;
+    return createVector(t00 * p.x + t01 * p.y, t10 * p.x + t11 * p.y);
+  }
   draw() {
     this.setStyle();
     line(this.x1, this.y1, this.x2, this.y2);
+  }
+  inside(x, y) {
+    let p1 = createVector(this.x1, this.y1);
+    let p2 = createVector(this.x2, this.y2);
+    let s = p5.Vector.sub(p2, p1);
+    let m = p5.Vector.add(p1, p2).div(2);
+    let th = s.heading();
+    let l = s.mag();
+    let margin = 5;
+    let r11 = createVector(-l / 2 - margin, -margin);
+    let h = 2 * margin;
+    let w = l + h;
+    let ms = createVector(x, y);
+    ms.sub(m);
+    ms = this.rotCCW(ms, th);
+    return Rect.inside(ms.x, ms.y, r11.x, r11.y, w, h);
   }
   serialize() {
     const base = super.serialize();
