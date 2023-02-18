93d8034d34168c3ebeb8c6ad9f5292de9541487f
Gianluigi Forte <koalakoker@gmail.com>
2023-02-18 11:21:29 +0100

    Manage key with cmd, alt, ctrl, shift

--- keyLogger.js
+++ keyLogger.js
@@ -2,6 +2,11 @@ class KeyLogger extends Subject {
   constructor() {
     super();
     this.state;
+    this.meta = false;
+    this.alt = false;
+    this.control = false;
+    this.shift = false;
+    this.key = "";
   }
   setState(state) {
     this.state = state;
@@ -11,10 +16,63 @@ class KeyLogger extends Subject {
     return this.state;
   }
   keyPressed() {
-    this.setState("key pressed: " + key);
+    switch (key) {
+      case "Meta":
+        this.meta = true;
+        break;
+      case "Alt":
+        this.alt = true;
+        break;
+      case "Control":
+        this.control = true;
+        break;
+      case "Shift":
+        this.shift = true;
+        break;
+
+      default:
+        this.key = key;
+        this.setState("key pressed: " + this.stateString());
+        break;
+    }
   }
   keyReleased() {
-    this.setState("Key released: " + key);
+    switch (key) {
+      case "Meta":
+        this.meta = false;
+        break;
+      case "Alt":
+        this.alt = false;
+        break;
+      case "Control":
+        this.control = false;
+        break;
+      case "Shift":
+        this.shift = false;
+        break;
+
+      default:
+        this.key = key;
+        this.setState("Key released: " + this.stateString());
+        break;
+    }
+  }
+  stateString() {
+    let str = "";
+    if (this.meta) {
+      str += "cmd+";
+    }
+    if (this.alt) {
+      str += "alt+";
+    }
+    if (this.control) {
+      str += "ctrl+";
+    }
+    if (this.shift) {
+      str += "shift+";
+    }
+    str += this.key;
+    return str;
   }
 }
 
