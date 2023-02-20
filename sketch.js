7a8dff00a09e4ce586b4ee38490dad3918e16c8e
Gianluigi Forte <koalakoker@gmail.com>
2023-02-20 07:00:16 +0100

    Testing keyLog and shortcut cmd+z

--- sketch.js
+++ sketch.js
@@ -2,20 +2,28 @@ function setup() {
   //createCanvas(window.innerWidth, window.innerHeight);
   const test = new Test();
   let p = createP();
+  p.position(0, 0);
+  let txt = "";
 
-  const shortCut1 = new ShortCut(
-    new KeyState("z", false, false, true, false),
-    (k) => {
-      p.html(k.toString());
-    }
-  );
+  new ShortCut(new KeyState("z", false, false, true, false), (k) => {
+    txt += k.toString() + "<br>";
+    p.html(txt);
+  });
 
-  const shortCut2 = new ShortCut(
-    new KeyState("Z", false, false, true, true),
-    (k) => {
-      p.html(k.toString());
-    }
-  );
+  new ShortCut(new KeyState("Z", false, false, true, true), (k) => {
+    txt += k.toString() + "<br>";
+    p.html(txt);
+  });
+
+  new ShortCut(new KeyState("z", true, false, false, false), (k) => {
+    txt += k.toString() + "<br>";
+    p.html(txt);
+  });
+
+  new ShortCut(new KeyState("z", true, false, false, true), (k) => {
+    txt += k.toString() + "<br>";
+    p.html(txt);
+  });
 }
 
 function draw() {
