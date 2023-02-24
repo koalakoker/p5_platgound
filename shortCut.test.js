273e4975faa1c00853b431f6a5997b7a014d610a
Gianluigi Forte <koalakoker@gmail.com>
2023-02-24 06:50:51 +0100

    Refactoring tests

--- shortCut.test.js
+++ shortCut.test.js
@@ -26,43 +26,45 @@ function keyEventGen(type, ks) {
 }
 
 function CheckKeyLoggerAndShortCutOpeartions() {
-  return new Promise((resolve, reject) => {
-    let activations = 0;
-    const expectedActivation = 7;
+  it("Check KeyLogger and ShortCut opeartions", () => {
+    return new Promise((resolve, reject) => {
+      let activations = 0;
+      const expectedActivation = 7;
 
-    const shortCut = new ShortCut(new KeyState("z").addCtrl(), (k) => {
-      if (k.isEqual(shortCut.activationKey)) {
-        activations++;
-      }
-    });
-    const shortCut2 = new ShortCut(
-      new KeyState("Z").addCtrl().addShift(),
-      (k) => {
-        if (k.isEqual(shortCut2.activationKey)) {
+      const shortCut = new ShortCut(new KeyState("z").addCtrl(), (k) => {
+        if (k.isEqual(shortCut.activationKey)) {
           activations++;
         }
-      }
-    );
+      });
+      const shortCut2 = new ShortCut(
+        new KeyState("Z").addCtrl().addShift(),
+        (k) => {
+          if (k.isEqual(shortCut2.activationKey)) {
+            activations++;
+          }
+        }
+      );
 
-    keyLogSimulatePress_shift_ctrl_z(); // Trigger
-    keyLogSimulatePress_ctrl_a();
-    keyLogSimulatePress_ctrl_z(); // Trigger
-    keyLogSimulatePress_shift_ctrl_z(); // Trigger
-    kl.detach(shortCut2);
-    keyLogSimulatePress_ctrl_z(); // Trigger
-    keyLogSimulatePress_shift_ctrl_z();
-    kl.detach(shortCut);
-    keyLogSimulatePress_ctrl_z();
-    kl.attach(shortCut);
-    keyLogSimulatePress_ctrl_a();
-    keyLogSimulatePress_ctrl_z(); // Trigger
-    keyLogSimulatePress_ctrl_z(); // Trigger
-    kl.attach(shortCut2);
-    keyLogSimulatePress_shift_ctrl_z(); // Trigger
-    if (activations === expectedActivation) {
-      resolve();
-    } else {
-      reject();
-    }
+      keyLogSimulatePress_shift_ctrl_z(); // Trigger
+      keyLogSimulatePress_ctrl_a();
+      keyLogSimulatePress_ctrl_z(); // Trigger
+      keyLogSimulatePress_shift_ctrl_z(); // Trigger
+      kl.detach(shortCut2);
+      keyLogSimulatePress_ctrl_z(); // Trigger
+      keyLogSimulatePress_shift_ctrl_z();
+      kl.detach(shortCut);
+      keyLogSimulatePress_ctrl_z();
+      kl.attach(shortCut);
+      keyLogSimulatePress_ctrl_a();
+      keyLogSimulatePress_ctrl_z(); // Trigger
+      keyLogSimulatePress_ctrl_z(); // Trigger
+      kl.attach(shortCut2);
+      keyLogSimulatePress_shift_ctrl_z(); // Trigger
+      if (activations === expectedActivation) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
   });
 }
--- test.js
+++ test.js
@@ -1,14 +1,4 @@
 const assert = chai.assert;
-
 describe("KeyLogger", keyLoggerTest);
-
-describe("KeyState", function () {
-  it("Check KeyState isEqual method", checkKeyStateIsEqualMethod);
-});
-
-describe("ShortCut", function () {
-  it(
-    "Check KeyLogger and ShortCut opeartions",
-    CheckKeyLoggerAndShortCutOpeartions
-  );
-});
+describe("KeyState", checkKeyStateIsEqualMethod);
+describe("ShortCut", CheckKeyLoggerAndShortCutOpeartions);
