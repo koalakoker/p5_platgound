144fbe3e1c6d30fb2465bcad5baaf8dbe18cefb8
Gianluigi Forte <koalakoker@gmail.com>
2023-03-02 06:05:06 +0100

    Add Rect State testting added

--- state/addRect.test.js
+++ state/addRect.test.js
@@ -1,5 +1,23 @@
 function addRectTests() {
   it("Add rect state tests", () => {
-    assert.notEqual(1, 1);
+    const tDrw = Drawing.getInstance();
+    tDrw.clear();
+    const sAddRect = new StateAddRect();
+    const firstPoint = { x: 400, y: 300 };
+    const secondPoint = { x: 455, y: 300 };
+    mouseX = firstPoint.x;
+    mouseY = firstPoint.y;
+    sAddRect.mousePressed();
+    mouseX = secondPoint.x;
+    mouseY = secondPoint.y;
+    sAddRect.mouseDragged();
+    sAddRect.mouseReleased();
+    assert.equal(tDrw.getElements().length, 1);
+    const newElem = tDrw.getElements()[0];
+    assert.isTrue(Rectangle.prototype.isPrototypeOf(newElem));
+    assert.equal(newElem.x1, firstPoint.x);
+    assert.equal(newElem.y1, firstPoint.y);
+    assert.equal(newElem.x2, 460);
+    assert.equal(newElem.y2, secondPoint.y);
   });
 }
