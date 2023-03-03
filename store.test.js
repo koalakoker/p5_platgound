c356d172a76428766fe9732f3d16715e2d0f98f7
Gianluigi Forte <koalakoker@gmail.com>
2023-03-03 05:47:40 +0100

    Testing store

--- store.test.js
+++ store.test.js
@@ -1,5 +1,31 @@
 function storeTests() {
   it("Store tests", () => {
-    assert.equal(1, 0);
+    const store = Store.getInstance();
+    store.clear();
+    assert.equal(store.currentIndex, -1);
+    assert.deepEqual(store.history, []);
+
+    const drw = Drawing.getInstance();
+    drw.clear();
+
+    const state1 = drw.serialize();
+    store.addState();
+    assert.equal(store.currentIndex, 0);
+    assert.notDeepEqual(store.history, []);
+    assert.deepEqual(store.history[0], state1);
+
+    drw.addNewElement(new Circle(100, 100, 100));
+    const state2 = drw.serialize();
+    store.addState();
+    console.log();
+    assert.deepEqual(store.history[1], state2);
+
+    assert.equal(drw.serialize(), state2);
+    store.moveToNextState();
+    assert.equal(drw.serialize(), state2);
+    store.moveToPreviousState();
+    assert.equal(drw.serialize(), state1);
+    store.moveToPreviousState();
+    assert.equal(drw.serialize(), state1);
   });
 }
