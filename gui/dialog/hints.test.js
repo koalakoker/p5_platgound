971ae22724ac082bc37fac8d31273fb89c5d947c
Gianluigi Forte <koalakoker@gmail.com>
2023-03-07 06:12:19 +0100

    Completed hints tests

--- gui/dialog/hints.test.js
+++ gui/dialog/hints.test.js
@@ -1,8 +1,17 @@
 function hintsTests() {
   it("Hints tests", () => {
+    const duration = 3000;
+    const fadeIn = 200;
+    const fadeOut = 200;
+    const margin = 10;
     const hints = new Hints();
     assert.isArray(hints.messages);
     const gui = Gui.getInstance();
-    //console.log(gui);
+    gui.diagMngr.display();
+    const msg = gui.diagMngr.activeDiag();
+    simulateWait(msg, duration + fadeIn + fadeOut);
+    assert.equal(gui.diagMngr.dialogs.length, 1);
+    simulateWait(msg, margin);
+    assert.equal(gui.diagMngr.dialogs.length, 0);
   });
 }
