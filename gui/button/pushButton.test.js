5dfceeea475a85c9f3d7f0e85d3cc5926781ed95
Gianluigi Forte <koalakoker@gmail.com>
2023-03-09 21:46:34 +0100

    PushButton test

--- gui/button/pushButton.test.js
+++ gui/button/pushButton.test.js
@@ -1,9 +1,17 @@
 function pushButtonTest() {
   it("Test", () => {
-    const pushButton = new PushButton(null, "", () => {
-      console.log("Pushed");
+    return new Promise((resolve, reject) => {
+      let push = 0;
+      const pushButton = new PushButton(null, "", () => {
+        push++;
+        if (push === 2) {
+          resolve();
+        }
+      });
+      assert.isTrue(isTypeOf(pushButton, PushButton));
+      pushButton.activate();
+      pushButton.mousePressed(pushButton.x + 5, pushButton.y + 5);
+      reject("Push check failed");
     });
-    console.log(pushButton);
-    pushButton.activate();
   });
 }
