6cf3e4ceab4c4c1036736f2d61a97f12392cfddc
Gianluigi Forte <koalakoker@gmail.com>
2023-03-11 19:54:14 +0100

    Fixed hint

--- gui/dialog/hints.js
+++ gui/dialog/hints.js
@@ -1,7 +1,7 @@
 class Hints {
   constructor() {
     this.messages = [];
-    this.messages.push("Use ctrl key to show and lock the main bar");
+    this.messages.push("Use ctrl+L key to show and lock the main bar");
     this.messages.push("Hold shift key to select multiple shapes");
     const gui = Gui.getInstance();
     gui.addMessage(
