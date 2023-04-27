fe313499cc638f5062cf6a11a36c827df33115e4
Gianluigi Forte <koalakoker@gmail.com>
2023-04-27 05:54:16 +0200

    Edit: extend selection with arrow key + shift

--- gui/edit/cursor.js
+++ gui/edit/cursor.js
@@ -2,6 +2,9 @@ class Cursor extends GElem {
   constructor(parent, size) {
     super(parent, 0, 0, 0, size);
     this.setEditPosition(0);
+    this.sel = false;
+    this.selStart = 0;
+    this.selStop = 0;
   }
   detach() {
     kl.detach(this.shortCut);
@@ -13,19 +16,63 @@ class Cursor extends GElem {
   setEditPosition(pos) {
     this.pos = pos;
   }
+
+  extendSelectionRight(pos) {
+    const nPos = pos || 1;
+    this.beforeExtendSelection();
+    this.pos2 += nPos;
+    this.afterExtendSelection();
+  }
+  extendSelectionLeft(pos) {
+    const nPos = pos || 1;
+    this.beforeExtendSelection();
+    this.pos2 -= nPos;
+    this.afterExtendSelection();
+  }
+  beforeExtendSelection() {
+    if (!this.sel) {
+      this.sel = true;
+      this.pos2 = this.pos;
+    }
+  }
+  afterExtendSelection() {
+    if (this.pos < this.pos2) {
+      this.selStart = this.pos;
+      this.selStop = this.pos2;
+    } else {
+      this.selStart = this.pos2;
+      this.selStop = this.pos;
+    }
+  }
+
   moveRight(pos) {
     const nPos = pos || 1;
-    this.pos += nPos;
+    if (this.sel) {
+      this.pos = this.selStop;
+      this.sel = false;
+    } else {
+      this.pos += nPos;
+    }
   }
   moveLeft(pos) {
     const nPos = pos || 1;
-    this.pos -= nPos;
+    if (this.sel) {
+      this.pos = this.selStart;
+      this.sel = false;
+    } else {
+      this.pos -= nPos;
+    }
+  }
+  isSelectionActive() {
+    return this.sel;
   }
   draw() {
-    const str = this.parent.editedText().substring(0, this.pos);
-    const x = this.getX() + p5js.textWidth(str);
-    const y = this.getY();
-    p5js.stroke(255, 0, 0);
-    p5js.line(x, y, x, y + this.h);
+    if (!this.sel) {
+      const str = this.parent.editedText().substring(0, this.pos);
+      const x = this.getX() + p5js.textWidth(str);
+      const y = this.getY();
+      p5js.stroke(255, 0, 0);
+      p5js.line(x, y, x, y + this.h);
+    }
   }
 }
--- gui/edit/editElement.js
+++ gui/edit/editElement.js
@@ -18,32 +18,51 @@ class EditElement extends GElem {
   setEditedText(text) {
     this.text = text;
   }
-  insertAtCursor(newTxt) {
-    const txt = this.editedText();
-    const pos = this.cursor.editPosition();
-    this.setEditedText(txt.slice(0, pos) + newTxt + txt.slice(pos));
-    this.cursor.moveRight(newTxt.lenght);
-  }
-  backspace() {
-    const txt = this.editedText();
-    const pos = this.cursor.editPosition();
-    this.setEditedText(txt.slice(0, pos - 1) + txt.slice(pos));
-    this.cursor.moveLeft();
-  }
+
   draw() {
     p5js.fill(0);
     p5js.stroke(255);
     p5js.rectMode(p5js.CORNER);
     p5js.rect(this.getX(), this.getY(), this.w, this.h);
 
+    this.drawText();
+    this.cursor.draw();
+  }
+  drawText() {
     p5js.textSize(this.textSize);
     p5js.textAlign(p5js.LEFT, p5js.TOP);
     p5js.stroke(255);
     p5js.fill(255);
-    p5js.text(this.text, this.getX(), this.getY());
+    if (this.cursor.isSelectionActive()) {
+      const before = this.stripBeforeSelection();
+      const selection = this.stripSelection();
+      const after = this.stripAfterSelection();
 
-    this.cursor.draw();
+      p5js.text(before, this.getX(), this.getY());
+      p5js.stroke(255, 0, 0);
+      p5js.fill(255, 0, 0);
+      p5js.text(selection, this.getX() + p5js.textWidth(before), this.getY());
+      p5js.stroke(255);
+      p5js.fill(255);
+      p5js.text(
+        after,
+        this.getX() + p5js.textWidth(before) + p5js.textWidth(selection),
+        this.getY()
+      );
+    } else {
+      p5js.text(this.text, this.getX(), this.getY());
+    }
   }
+  stripBeforeSelection() {
+    return this.text.slice(0, this.cursor.selStart);
+  }
+  stripSelection() {
+    return this.text.slice(this.cursor.selStart, this.cursor.selStop);
+  }
+  stripAfterSelection() {
+    return this.text.slice(this.cursor.selStop);
+  }
+
   onClose() {
     this.cursor.detach();
   }
@@ -77,7 +96,30 @@ class EditElement extends GElem {
     if (key !== "") {
       if (key.length === 1) {
         this.insertAtCursor(key);
+      } else {
+        const pos = this.cursor.editPosition();
+        if (k.getKey() === "ArrowRight") {
+          if (pos < this.editedText().length) {
+            this.cursor.extendSelectionRight();
+          }
+        } else if (k.getKey() === "ArrowLeft") {
+          if (pos > 0) {
+            this.cursor.extendSelectionLeft();
+          }
+        }
       }
     }
+  }
+  insertAtCursor(newTxt) {
+    const txt = this.editedText();
+    const pos = this.cursor.editPosition();
+    this.setEditedText(txt.slice(0, pos) + newTxt + txt.slice(pos));
+    this.cursor.moveRight(newTxt.lenght);
+  }
+  backspace() {
+    const txt = this.editedText();
+    const pos = this.cursor.editPosition();
+    this.setEditedText(txt.slice(0, pos - 1) + txt.slice(pos));
+    this.cursor.moveLeft();
   }
 }
